6d6df41a9632c2f13fbdbac3af1c07550fd4a779
dragochar <zacyungblut@gmail.com>
2023-02-10 16:46:40 -0500

    lol

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,6 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={netGalaLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={propsLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Winder Support</h1>
       <p className="read-the-docs">
